764d2954bc5f30b01064c7c29f58d158374fe85f
dcabanas <diniscabanas@gmail.com>
2023-12-13 21:17:18 +0000

    Next.js Project Time (page-routing) section done

--- project-routing/pages/events/[id]/index.js
+++ project-routing/pages/events/[id]/index.js
@@ -1,10 +1,30 @@
 import { useRouter } from 'next/router'
+import { getEventById } from '../../../dummy-data'
+import EventSummary from '../../../components/events/event-detail/event-summary'
+import EventLogistics from '../../../components/events/event-detail/event-logistics'
+import EventContent from '../../../components/events/event-detail/event-content'
+import ErrorAlert from '../../../components/ui/error-alert'
 
 export default function EventDetailPage() {
     const router = useRouter()
     console.log(router.query)
 
+    const event = getEventById(router.query.id)
+    if (!event) {
+        return (
+            <ErrorAlert>
+                <p>No event found!</p>
+            </ErrorAlert>
+        )
+    }
+
     return (
-        <h1>The Event Detail Page</h1>
+        <>
+            <EventSummary title={event.title}/>
+            <EventLogistics date={event.date} address={event.location} image={event.image} imageAlt={event.alt}/>
+            <EventContent>
+                <p>{event.description}</p>
+            </EventContent>
+        </>
     )
 }
--- project-routing/pages/events/index.js
+++ project-routing/pages/events/index.js
@@ -1,5 +1,22 @@
+import { getAllEvents } from '../../dummy-data'
+import EventList from '../../components/events/event-list'
+import EventsSearch from '../../components/events/events-search'
+import { useRouter } from 'next/router'
+
 export default function EventsPage() {
+    const events = getAllEvents()
+    const router = useRouter()
+
+    function findEventsHandler(year, month) {
+        const fullPath = `/events/${year}/${month}`
+
+        router.push(fullPath)
+    }
+
     return (
-        <h1>The Events Page</h1>
+        <>
+            <EventsSearch onSearch={findEventsHandler}/>
+            <EventList items={events} />
+        </>
     )
 }
