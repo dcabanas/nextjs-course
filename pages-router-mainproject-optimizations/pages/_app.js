e8f2fc41ce325d981317a9d0f481d01356861f68
dcabanas <diniscabanas@gmail.com>
2023-12-17 12:37:15 +0000

    Next.js Project Time (optimizing) section done

--- pages-router-mainproject-optimizations/pages/_app.js
+++ pages-router-mainproject-optimizations/pages/_app.js
@@ -1,9 +1,19 @@
 import Layout from '../components/layout/layout';
 import '../styles/globals.css';
+import Head from 'next/head'
 
+/*
+  we can set general metadata here that will be shown across all pages
+  but if a page has its specific Head/metadata, will override the general one
+ */
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+        <Head>
+            <title>Next Events</title>
+            <meta name="description" content="NextJS Events"/>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+        </Head>
       <Component {...pageProps} />
     </Layout>
   );
