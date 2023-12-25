c1becf3d6e124817eb692b97998be8f19e04a0a0
dcabanas <diniscabanas@gmail.com>
2023-12-25 11:31:24 +0000

    Next.js Complete App Example (page-routing) section WIP

---
+++ pages-router-complete-app-example/components/contact/contact-form.js
@@ -0,0 +1,49 @@
+import classes from './contact-form.module.css'
+import { useState } from 'react'
+
+export default function ContactForm() {
+    const [enteredEmail, setEnteredEmail] = useState('')
+    const [enteredName, setEnteredName] = useState('')
+    const [enteredMessage, setEnteredMessage] = useState('')
+
+    function sendMessageHandler(event) {
+        event.preventDefault()
+
+        fetch('/api/contact', {
+            method: 'POST',
+            body: JSON.stringify({
+                email: enteredEmail,
+                name: enteredName,
+                message: enteredMessage
+            })
+        })
+    }
+
+    return (
+        <section className={classes.contact}>
+            <h1>How can I help you?</h1>
+            <form className={classes.form} onSubmit={sendMessageHandler}>
+                <div className={classes.controls}>
+                    <div className={classes.control}>
+                        <label htmlFor="email">Your email</label>
+                        <input type="email" id="email" required value={enteredEmail}
+                               onChange={event => setEnteredEmail(event.target.value)}/>
+                    </div>
+                    <div className={classes.control}>
+                        <label htmlFor="name">Your Name</label>
+                        <input type="text" id="name" required value={enteredName}
+                               onChange={event => setEnteredName(event.target.value)}/>
+                    </div>
+                </div>
+                <div className={classes.control}>
+                    <label htmlFor="message">Your Message</label>
+                    <textarea id="message" rows="5" required value={enteredMessage}
+                              onChange={event => setEnteredMessage(event.target.value)}></textarea>
+                </div>
+                <div className={classes.actions}>
+                    <button>Send message</button>
+                </div>
+            </form>
+        </section>
+    )
+}
