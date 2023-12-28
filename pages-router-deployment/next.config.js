79b19c0ad446436ade3438f896239ad597afa648
dcabanas <dcabanas@opensoft.pt>
2023-12-28 10:06:06 +0000

    Next.js Deployment (page-routing) section done

--- pages-router-deployment/next.config.js
+++ pages-router-deployment/next.config.js
@@ -1,5 +1,10 @@
 const {PHASE_DEVELOPMENT_SERVER} = require('next/constants')
 
+/*
+    It is recommended to inject and use ENV variables
+    directly in Vercel (or any other deployment server)
+    instead of using this file
+ */
 module.exports = (phase) => {
     if (phase === PHASE_DEVELOPMENT_SERVER) {
         return {
